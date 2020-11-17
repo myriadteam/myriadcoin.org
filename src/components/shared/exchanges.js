c8bbc12e99d3b6957db2993d237b5dcfe1c4e4b3
Emil Karlsson <emil.karl@gmail.com>
2020-11-17 21:02:57 +0100

    Fix grid for exchange links

--- src/components/shared/exchanges.js
+++ src/components/shared/exchanges.js
@@ -8,17 +8,17 @@ const Exchanges = () => {
     return (
       <a
         href={url}
-        tw="flex items-center justify-center rounded-full bg-light-grey w-full h-auto pt-full text-xxs text-grey overflow-hidden m-4"
+        tw="rounded-full bg-light-grey w-24 h-24 flex justify-center items-center text-xxs text-grey overflow-hidden p-2"
         title={name}
         target="_blank"
         rel="noopener noreferrer"
       >
-        {logo && <img src={logo} alt={name} title={name} />}
+        {logo && <img src={logo} alt={name} title={name} tw="rounded-full" />}
       </a>
     )
   }
   return (
-    <div tw="flex -mx-4">
+    <div tw="grid grid-cols-3 sm:grid-cols-5 lg:grid-cols-9 gap-5">
       {exchanges.map(exchange => renderExchange(exchange))}
     </div>
   )
