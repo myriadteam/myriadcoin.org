8d96ea4d57c1083d7748978fba93b44784fb1496
Emil Karlsson <emil.karl@gmail.com>
2020-11-17 23:10:03 +0100

    Add bocy block component

---
+++ src/components/shared/body-block.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { useTranslation, Trans } from "react-i18next"
+import tw from "twin.macro"
+import {
+  PageContainer,
+  MediumBoldText,
+  BodyText,
+  LinkButton,
+} from "../../common/elements"
+
+const BodyBlock = ({ translationKey }) => {
+  const { t } = useTranslation()
+  const link = t(`${translationKey}.link`, { returnObjects: true })
+  console.log("link", link)
+  return (
+    <div tw="max-w-2xl mb-32 last:mb-0">
+      <MediumBoldText>{t(`${translationKey}.title`)}</MediumBoldText>
+      <Trans
+        i18nKey={`${translationKey}.body`}
+        components={[<BodyText tw="mb-6" />]}
+      />
+      {link && link.title && link.url && (
+        <LinkButton href={link.url} tw="mt-10 mb-0">
+          {link.title}
+        </LinkButton>
+      )}
+    </div>
+  )
+}
+
+export default BodyBlock
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import tw from "twin.macro"
 import SEO from "../components/seo"
+import BodyBlock from "../components/shared/body-block"
 import iconArrowBlack from "../svgs/icons/arrow-forward.svg"
 
 import {
@@ -26,21 +27,18 @@ const AboutPage = () => {
         </div>
       </PageContainer>
       <PageContainer tw="py-24 px-6 sm:py-48 sm:px-0">
-        <div tw="max-w-2xl">
-          <MediumBoldText>{t("about.top.title")}</MediumBoldText>
-          <Trans
-            i18nKey="about.top.body"
-            components={[<BodyText tw="mb-6" />]}
-          />
-        </div>
+        <BodyBlock translationKey="about.top" />
       </PageContainer>
       <div tw="bg-black text-white">
         <PageContainer tw="py-24 px-6 sm:py-48 sm:px-0">
           <MediumBoldText tw="mb-24">{t("about.history.title")}</MediumBoldText>
 
           <div tw="flex flex-col sm:-mx-8">
-            {historyItems.map(item => (
-              <div tw="mb-16 sm:mb-10 sm:max-w-2xl items-stretch justify-center sm:even:self-end sm:even:text-right flex flex-col sm:flex-row sm:even:flex-row-reverse">
+            {historyItems.map((item, index) => (
+              <div
+                key={`history-${index}`}
+                tw="mb-16 sm:mb-10 sm:max-w-2xl items-stretch justify-center sm:even:self-end sm:even:text-right flex flex-col sm:flex-row sm:even:flex-row-reverse"
+              >
                 <div tw="rounded-full w-full h-4 sm:h-auto sm:w-8 bg-gradient-to-r sm:bg-gradient-to-b from-blue-light flex-shrink-0 sm:mx-8 mb-8 sm:mb-0" />
                 <div>
                   <BodyBoldText tw="mb-5 leading-none">
@@ -53,6 +51,10 @@ const AboutPage = () => {
           </div>
         </PageContainer>
       </div>
+      <PageContainer tw="py-24 px-6 sm:py-48 sm:px-0">
+        <BodyBlock translationKey="about.community" />
+        <BodyBlock translationKey="about.fair" />
+      </PageContainer>
     </>
   )
 }
