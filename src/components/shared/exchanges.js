5c7de9c48b4513b268c8d84defd509fb93aa8478
Emil Karlsson <emil.karl@gmail.com>
2020-12-27 12:44:39 +0100

    Each exchange must have a key

--- src/components/shared/exchanges.js
+++ src/components/shared/exchanges.js
@@ -4,9 +4,9 @@ import { exchanges } from "../../common/exchanges"
 import { MediumBoldText } from "../../common/elements"
 
 const Exchanges = () => {
-  const renderExchange = ({ name, logo, url }) => {
+  const renderExchange = ({ name, logo, url }, index) => {
     return (
-      <div tw="flex items-center justify-center">
+      <div tw="flex items-center justify-center" key={`exchange-${index}`}>
         <a
           href={url}
           tw="rounded-full bg-light-grey w-18 h-18 flex justify-center items-center text-xxs text-grey overflow-hidden p-2"
@@ -21,7 +21,7 @@ const Exchanges = () => {
   }
   return (
     <div tw="rounded shadow-wide flex-shrink-0 sm:ml-16 bg-white grid grid-cols-3 gap-2 sm:grid-cols-2 sm:gap-4 px-6 py-8">
-      {exchanges.map(exchange => renderExchange(exchange))}
+      {exchanges.map((exchange, index) => renderExchange(exchange, index))}
     </div>
   )
 }
