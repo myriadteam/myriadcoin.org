b3d9feb05496d82e9b9bb73eef646131295cba38
rikardwissing <rikard.wissing@teamtailor.com>
2021-01-15 16:18:28 +0100

    Add a bubble tail

--- src/components/bubble.js
+++ src/components/bubble.js
@@ -1,8 +1,9 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
+import BubbleSvg from "../svgs/bubble.inline.svg"
 
 const Container = styled.div`
-  ${tw`inline-flex rounded-full py-4 px-8 text-white text-sm font-semibold leading-extra-tight sm:(py-6 px-12 text-md)`}
+  ${tw`inline-flex relative rounded-full py-4 px-8 text-white text-sm font-semibold leading-extra-tight sm:(py-6 px-12 text-md)`}
   ${({ color }) =>
     color === "blue" ? tw`bg-bubble-blue` : tw`bg-gradient-t-black-grey`}
 `
@@ -11,6 +12,7 @@ const Bubble = ({ children, className, color }) => {
   return (
     <Container className={className} color={color}>
       {children}
+      <BubbleSvg tw="absolute bottom-0 left-0" />
     </Container>
   )
 }
