b2cf485835bfc718ce301a841cdcc8c3897b0ab7
Emil Karlsson <emil.karl@gmail.com>
2021-01-19 18:19:43 +0100

    Remove warning for exact link check

--- src/components/header-link.js
+++ src/components/header-link.js
@@ -3,17 +3,13 @@ import PropTypes from "prop-types"
 import { Link as GatsbyLink } from "gatsby"
 
 export default function Link(props) {
-  const { exact, className, activeClassName, children } = props
+  const { className, activeClassName, children } = props
 
   return (
     <GatsbyLink
       {...props}
-      getProps={({ isCurrent, isPartiallyCurrent }) => ({
-        className: [
-          className,
-          exact && isCurrent ? activeClassName : "",
-          !exact && isPartiallyCurrent ? activeClassName : "",
-        ]
+      getProps={({ isPartiallyCurrent }) => ({
+        className: [className, isPartiallyCurrent ? activeClassName : ""]
           .join(" ")
           .trim(),
       })}
@@ -24,14 +20,12 @@ export default function Link(props) {
 }
 
 Link.propTypes = {
-  exact: PropTypes.bool,
   className: PropTypes.string,
   activeClassName: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
 
 Link.defaultProps = {
-  exact: false,
   activeClassName: "active",
   className: "",
 }
