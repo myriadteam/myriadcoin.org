d42b6e84fc6dac74638a6f73452c70b3055cab90
rikardwissing <rikard.wissing@teamtailor.com>
2021-01-27 03:07:49 +0100

    Update line-graph.js

--- src/components/graphs/line-graph.js
+++ src/components/graphs/line-graph.js
@@ -26,16 +26,16 @@ function LineGraph() {
   ])
 
   return (
-    <div tw="bg-white dark:bg-dark-bg shadow-wide px-16 py-18 rounded">
+    <div tw="bg-white dark:bg-dark-bg shadow-wide px-6 py-6 sm:px-16 sm:py-18 rounded">
       <MediumBoldText>Block weights</MediumBoldText>
       <div tw="flex flex-row text-grey font-normal">
-        <div tw="flex flex-col justify-between py-4 pr-6 pb-14">
+        <div tw="flex flex-col justify-between pt-1 pr-2 pb-8 sm:pr-6 sm:pt-4 sm:pb-14">
           <div>{parsedData.maxY}</div>
           <div>{parsedData.minY}</div>
         </div>
         <div tw="flex-grow">
           <LineGraphContent parsedData={parsedData} />
-          <div tw="flex flex-row justify-between px-10 pt-8">
+          <div tw="flex flex-row justify-between px-2 sm:px-10 pt-3 sm:pt-8">
             <div>{parsedData.minX}</div>
             <div>
               {parsedData.minX + (parsedData.maxX - parsedData.minX) / 3}
