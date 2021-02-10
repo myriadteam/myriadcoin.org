daf95b8282c15a3fe7514c0b1c7493e8604ef3be
rikardwissing <rikard.wissing@teamtailor.com>
2021-02-10 10:15:40 +0100

    Update stacked-plot.js

--- src/components/graphs/stacked-plot.js
+++ src/components/graphs/stacked-plot.js
@@ -1,34 +1,32 @@
 import React from "react"
 
 const StackedPlot = ({ parsedData, stackColors }) => {
-  const { stackedData, exactData, x, y, width } = parsedData
+  const { stackedData, exactData, width } = parsedData
 
   const barWidth = (0.8 * width) / exactData.length
 
-  return stackedData.map((stack, stackI) => {
-    return (
-      <g key={stackI} fill={stackColors[stackI]}>
-        {stack.map(d => {
-          const [from, to] = d
-          const { data } = d
+  return stackedData.map((stack, stackI) => (
+    <g key={stackI} fill={stackColors[stackI]}>
+      {stack.slice().map(d => {
+        const [from, to] = d
+        const { data } = d
 
-          if (from === to) {
-            return null
-          }
+        if (from === to) {
+          return null
+        }
 
-          return (
-            <rect
-              key={data.x}
-              x={x(data.x) - barWidth / 2}
-              y={from}
-              width={barWidth}
-              height={y(from) - y(to)}
-            />
-          )
-        })}
-      </g>
-    )
-  })
+        return (
+          <rect
+            key={data.x}
+            x={data.x - barWidth / 2}
+            y={from}
+            width={barWidth}
+            height={to - from}
+          />
+        )
+      })}
+    </g>
+  ))
 }
 
 export default React.memo(StackedPlot)
