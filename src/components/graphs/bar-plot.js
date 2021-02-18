5fc839f24b5e91d993e3a909c3258a53dfe47c13
rikardwissing <rikard.wissing@teamtailor.com>
2021-02-18 18:11:38 +0100

    Paginate barplots (big performance gains)

--- src/components/graphs/bar-plot.js
+++ src/components/graphs/bar-plot.js
@@ -1,25 +1,54 @@
-import React from "react"
+import React, { useState, useRef } from "react"
+import { animated, interpolate } from "react-spring"
+import { useGraphZoomPan } from "./zoom-pan-context"
 
 const BarPlot = ({ parsedData, barPlotKeys, barPlotColors }) => {
+  const { offsetX, period } = useGraphZoomPan()
+
+  const [slice, setSlice] = useState()
+  const lastVal = useRef(0)
+  const lastPeriod = useRef(0)
+
   const { exactData, x, y, width } = parsedData
 
   const containerBarWidth = (0.8 * width) / exactData.length
   const barWidth = containerBarWidth / barPlotKeys.length
 
-  return exactData.map((d, di) => {
-    return barPlotKeys.map((barKey, barI) => {
-      return (
-        <rect
-          key={barKey + di}
-          x={x(d.x) - containerBarWidth / 2 + barWidth * barI}
-          y={0}
-          width={barWidth}
-          height={y(0) - y(d[barKey])}
-          fill={barPlotColors[barI]}
-        />
-      )
-    })
-  })
+  return (
+    <animated.g
+      data={interpolate([offsetX, period], offsetX => {
+        const diff = Math.abs(lastVal.current - offsetX)
+        const page = parseInt(period.animation.to * 0.3, 10)
+        if (diff > page || lastPeriod.current !== period.animation.to) {
+          lastVal.current = parseInt(offsetX, 10)
+          lastPeriod.current = period.animation.to
+
+          setSlice({
+            start: Math.max(lastVal.current - page, 0),
+            end: Math.max(lastVal.current + period.animation.to + page, 0),
+          })
+        }
+      })}
+    >
+      {barPlotKeys.map((barKey, barI) => {
+        return (
+          <g key={barKey} fill={barPlotColors[barI]}>
+            {slice !== undefined &&
+              exactData.slice(slice.start, slice.end).map((d, di) => {
+                return (
+                  <rect
+                    key={barKey + (slice.start + di)}
+                    x={x(d.x) - containerBarWidth / 2 + barWidth * barI}
+                    width={barWidth}
+                    height={y(0) - y(d[barKey])}
+                  />
+                )
+              })}
+          </g>
+        )
+      })}
+    </animated.g>
+  )
 }
 
 export default React.memo(BarPlot)
--- src/components/graphs/stacked-plot.js
+++ src/components/graphs/stacked-plot.js
@@ -1,32 +1,59 @@
-import React from "react"
+import React, { useState, useRef } from "react"
+import { animated, interpolate } from "react-spring"
+import { useGraphZoomPan } from "./zoom-pan-context"
 
 const StackedPlot = ({ parsedData, stackColors }) => {
+  const { offsetX, period } = useGraphZoomPan()
+
+  const [slice, setSlice] = useState()
+  const lastVal = useRef(0)
+  const lastPeriod = useRef(0)
+
   const { stackedData, exactData, width } = parsedData
 
   const barWidth = (0.8 * width) / exactData.length
 
-  return stackedData.map((stack, stackI) => (
-    <g key={stackI} fill={stackColors[stackI]}>
-      {stack.slice().map(d => {
-        const [from, to] = d
-        const { data } = d
+  return (
+    <animated.g
+      data={interpolate([offsetX, period], offsetX => {
+        const diff = Math.abs(lastVal.current - offsetX)
+        const page = parseInt(period.animation.to * 0.3, 10)
+        if (diff > page || lastPeriod.current !== period.animation.to) {
+          lastVal.current = parseInt(offsetX, 10)
+          lastPeriod.current = period.animation.to
 
-        if (from === to) {
-          return null
+          setSlice({
+            start: Math.max(lastVal.current - page, 0),
+            end: Math.max(lastVal.current + period.animation.to + page, 0),
+          })
         }
-
-        return (
-          <rect
-            key={data.x}
-            x={data.x - barWidth / 2}
-            y={from}
-            width={barWidth}
-            height={to - from}
-          />
-        )
       })}
-    </g>
-  ))
+    >
+      {stackedData.map((stack, stackI) => (
+        <g key={stackI} fill={stackColors[stackI]}>
+          {slice !== undefined &&
+            stack.slice(slice.start, slice.end).map(d => {
+              const [from, to] = d
+              const { data } = d
+
+              if (from === to) {
+                return null
+              }
+
+              return (
+                <rect
+                  key={data.x}
+                  x={data.x - barWidth / 2}
+                  y={from}
+                  width={barWidth}
+                  height={to - from}
+                />
+              )
+            })}
+        </g>
+      ))}
+    </animated.g>
+  )
 }
 
 export default React.memo(StackedPlot)
